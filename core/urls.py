eb8c3aa88eeabba4f2960baa31ba99b5ec0c13a4
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-03-06 14:23:49 +0200

    added all files

---
+++ core/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path,include
+from  . import views
+from core import views as user_views
+from django.contrib.auth import logout, views as auth_views
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib import admin
+
+
+
+urlpatterns = [
+    
+    path('', views.index, name='index'),
+    path('register/', views.registerUser, name='register'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html'), name='logout'),
+#    path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html'), name=logout),
+    path('feed/', views.feed, name='feed'),
+    path('followweb/<username>/', views.followweb, name='followweb'),
+    path('unfollowweb/<username>/', views.unfollowweb, name='unfollowweb'),
+    path('profile/<username>/', views.profile,name='profile'),
+    path('postweb/<username>/', views.postweb,name='postweb'),
+    path('commentweb/<username>/<post_id>/', views.commentweb,name='commentweb'),
+    path('welcome/', views.welcome, name='welcome'),
+    path('search', user_views.search, name='search'),
+
+]
